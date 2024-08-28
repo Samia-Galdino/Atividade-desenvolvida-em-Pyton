942ce6f83549eed648cb0f06a9049918cf988a64
Samia-Galdino <samiagaldino1212@gmail.com>
2024-08-28 17:27:32 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,20 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Samira") # comentário a linha do código
+print('Samira')
+print('Sâmia "Samira" Vitória')
+print('Sâmia "Samira" Vitória')
+print("Sâmia \"Samira\" Vitória" )
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
