3abd9875e81addc266048b507479e30b31156689
Samia-Galdino <samiagaldino1212@gmail.com>
2024-09-11 17:10:51 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Samira"
+idade = 17
+altura = 1.70
+
+# exibir mensagem na tela
+print("nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
