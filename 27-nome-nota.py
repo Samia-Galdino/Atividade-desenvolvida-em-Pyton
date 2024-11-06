0e1eb7578f3a6cb25fe93b77c479f648b1bd642f
Samia-Galdino <samiagaldino1212@gmail.com>
2024-11-06 17:13:34 -0300

    Add files via upload

---
+++ 27-nome-nota.py
@@ -0,0 +1,8 @@
+# Cria listas com os nomes e notas dos alunos
+nomes = ["Ana", "Bruno", "Carla", "Diego", "Eva"]
+notas = [8, 9, 5, 10, 7]
+
+# Mostra o nome e a nota de cada aluno
+print("Lista de Alunos e suas Notas:")
+for i in range(5):
+    print("Aluno:", nomes[i], "- Nota:", notas[i])
